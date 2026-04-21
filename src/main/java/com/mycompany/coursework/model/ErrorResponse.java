d72503cc714c737dad6f93ec7d9afa78a8a31cdd
TharinPerera04 <tharin.pererad@gmail.com>
2026-04-22 00:18:35 +0530

    create ErrorResponse model for API error handling

---
+++ src/main/java/com/mycompany/coursework/model/ErrorResponse.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.coursework.model;
+
+/**
+ *
+ * @author User
+ */
+public class ErrorResponse {
+
+    private int status;
+    private String message;
+
+    public ErrorResponse() {}
+
+    public ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
