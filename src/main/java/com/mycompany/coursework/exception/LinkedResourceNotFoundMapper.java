76d92614c5cd51149d09ff24ff7c1d33a1ab7391
TharinPerera04 <tharin.pererad@gmail.com>
2026-04-22 01:01:42 +0530

    creating SensorUnavailableMapper class

--- src/main/java/com/mycompany/coursework/exception/LinkedResourceNotFoundMapper.java
+++ src/main/java/com/mycompany/coursework/exception/LinkedResourceNotFoundMapper.java
@@ -3,11 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.coursework.exception;
-
+import com.mycompany.coursework.model.ErrorResponse;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+@Provider
 /**
  *
  * @author User
  */
-public class LinkedResourceNotFoundMapper {
-    
+public class LinkedResourceNotFoundMapper implements ExceptionMapper<LinkedResourceNotFoundException> {
+
+    @Override
+    public Response toResponse(LinkedResourceNotFoundException ex) {
+        return Response.status(422)
+                .entity(new ErrorResponse(422, ex.getMessage()))
+                .build();
+    }
 }
