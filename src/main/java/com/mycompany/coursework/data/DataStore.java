e72be59a55cc25e751bf26f1251eae039be00552
TharinPerera04 <tharin.pererad@gmail.com>
2026-04-22 00:01:32 +0530

    updating DataStore class

--- src/main/java/com/mycompany/coursework/data/DataStore.java
+++ src/main/java/com/mycompany/coursework/data/DataStore.java
@@ -3,9 +3,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.coursework.data;
+
 import com.mycompany.coursework.model.Room;
 import com.mycompany.coursework.model.Sensor;
+import com.mycompany.coursework.model.SensorReading;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 /**
  *
@@ -14,4 +18,7 @@ import java.util.Map;
 public class DataStore {
     public static Map<String, Room> rooms = new HashMap<>();
     public static Map<String, Sensor> sensors = new HashMap<>();
+    
+        // key = sensorId, value = list of readings for that sensor
+    public static Map<String, List<SensorReading>> readings = new HashMap<>();
 }
