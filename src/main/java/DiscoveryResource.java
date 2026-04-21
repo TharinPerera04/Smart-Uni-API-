42c892a86b75a4e9479f579da5d7fe0ce5caefc5
TharinPerera04 <tharin.pererad@gmail.com>
2026-04-21 18:19:11 +0530

    adding API endpoint DiscoveryResource class

--- src/main/java/DiscoveryResource.java
+++ src/main/java/DiscoveryResource.java
@@ -2,11 +2,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+package com.mycompany.coursework.resources;
 /**
  *
  * @author User
  */
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
 public class DiscoveryResource {
-    
-}
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, Object> getApiInfo() {
+        Map<String, Object> response = new LinkedHashMap<>();
+
+response.put("name", "Smart Campus Sensor & Room Management API");
+        response.put("version", "v1");
+        response.put("status", "running");
