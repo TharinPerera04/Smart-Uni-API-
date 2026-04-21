01ce7562c955c3ceaddf038b4f0daf08e73eae31
TharinPerera04 <tharin.pererad@gmail.com>
2026-04-22 01:15:29 +0530

    implement logging filter for API requests and responses

--- src/main/java/com/mycompany/coursework/filter/LoggingFilter.java
+++ src/main/java/com/mycompany/coursework/filter/LoggingFilter.java
@@ -3,11 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.coursework.filter;
-
+import javax.ws.rs.container.*;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.util.logging.Logger;
+@Provider
 /**
  *
  * @author User
  */
-public class LoggingFilter {
-    
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private static final Logger LOGGER = Logger.getLogger(LoggingFilter.class.getName());
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        LOGGER.info("Request: " + requestContext.getMethod() + " " + requestContext.getUriInfo().getRequestUri());
+    }
+
+    @Override
+    public void filter(ContainerResponseContext responseContext) throws IOException {
+        LOGGER.info("Response Status: " + responseContext.getStatus());
+    }
 }
