f7d8108e31f6dd14f8a8a92db9a85def53fc92e1
TharinPerera04 <tharin.pererad@gmail.com>
2026-04-22 00:03:40 +0530

    implement sensor readings API with GET and POST

--- src/main/java/com/mycompany/coursework/resources/SensorReadingResource.java
+++ src/main/java/com/mycompany/coursework/resources/SensorReadingResource.java
@@ -3,11 +3,74 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.coursework.resources;
+import com.mycompany.coursework.data.DataStore;
+import com.mycompany.coursework.model.Sensor;
+import com.mycompany.coursework.model.SensorReading;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 /**
  *
  * @author User
  */
 public class SensorReadingResource {
-    
+
+    private final String sensorId;
+
+    public SensorReadingResource(String sensorId) {
+        this.sensorId = sensorId;
+    }
+
+    @GET
+    public Response getReadings() {
+        Sensor sensor = DataStore.sensors.get(sensorId);
+
+        if (sensor == null) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("Sensor not found")
+                    .build();
+        }
+
+        List<SensorReading> sensorReadings = DataStore.readings.get(sensorId);
+
+        if (sensorReadings == null) {
+            sensorReadings = new ArrayList<>();
+        }
+
+        return Response.ok(sensorReadings).build();
+    }
+
+    @POST
+    public Response addReading(SensorReading reading) {
+        Sensor sensor = DataStore.sensors.get(sensorId);
+
+        if (sensor == null) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("Sensor not found")
+                    .build();
+        }
+
+        List<SensorReading> sensorReadings = DataStore.readings.get(sensorId);
+
+        if (sensorReadings == null) {
+            sensorReadings = new ArrayList<>();
+            DataStore.readings.put(sensorId, sensorReadings);
+        }
+
+        sensorReadings.add(reading);
+
+        // important coursework requirement:
+        // update parent sensor currentValue
+        sensor.setCurrentValue(reading.getValue());
+
+        return Response.status(Response.Status.CREATED)
+                .entity(reading)
+                .build();
+    }
 }
